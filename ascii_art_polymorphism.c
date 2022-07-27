27a3fb7248cab2745bf31ae9dd2ae2ccecdbb483
David Anderson <david.anderson@apollo-fire.com>
2022-07-27 15:00:44 +0100

    First C code implemented for project, current code prints out saturn & earth in ascii format, added comment for suggestion of base struct for polymorphism

--- ascii_art_polymorphism.c
+++ ascii_art_polymorphism.c
@@ -1,1 +1,46 @@
 /* Template file to get started with, going with C programming for now */ 
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+
+    printf(" \n\n     Saturn:\n\n");
+
+    printf("         ,MMM8&&&. \n");
+    printf("    _...MMMMM88&&&&..._ \n");
+    printf(" .::'''MMMMM88&&&&&&'''::. \n");
+    printf(" ::    MMMMM88&&&&&&    ::\n");
+    printf("'::....MMMMM88&&&&&&....::'\n ");
+    printf("  `''''MMMMM88&&&&''''`   \n");
+    printf("         'MMM8&&&'  \n");
+
+    printf("\n\n");
+
+    printf("      Earth: \n\n");
+
+    printf("         _____   \n");
+    printf("     ,-:` \\;',`'-, \n");
+    printf("   .'-;_,;  ':-;_,'. \n");
+    printf("  /;   '/    ,  _`.-\\ \n");
+    printf(" | '`. (`     /` ` \\`| \n");
+    printf(" |:.  `\\`-.   \\_   / | \n");
+    printf(" |     (   `,  .`\\ ;'| \n");
+    printf("  \\     | .'     `-'/ \n");
+    printf("   `.   ;/        .'   \n");
+    printf("     `'-._____. ' \n");
+
+    printf("\n\n");
+
+
+    /* Planet specs */
+
+    /* proposed base struct
+    * Orbit time
+    * Day length 
+    * Current speed
+    * Mass
+    * Atmosphere type
+    */
+   
+   return 0;
+}
